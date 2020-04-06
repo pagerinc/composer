38513faf732ddff406ce6ae03b6a71095cab3eb4
Tim Costa <tim@timcosta.io>
2020-04-06 09:46:23 -0400

    remove touch line

--- setup-dnsmasq.sh
+++ setup-dnsmasq.sh
@@ -6,4 +6,3 @@ echo 'port=53' >> $(brew --prefix)/etc/dnsmasq.conf
 sudo brew services start dnsmasq
 sudo mkdir -v /etc/resolver
 sudo echo 'nameserver 127.0.0.1' >> /etc/resolver/localhost
-sudo touch /etc/resolver/localhost
