91cc17c2a13a4639d0227e1409bc1b4eb2a7265c
Tim Costa <tim.costa@pager.com>
2021-03-10 10:18:50 -0500

    feat: update setup to support local docker-compose rfc [UE-1204] (#17)
    * feat: update setup to support local docker-compose rfc
    
    * fix arg
    
    * remove touch line
    
    * update README
    
    * wrap ca add in an if for linux users
    
    Co-authored-by: Oscar Calderon <70549595+oscarcalderonpager@users.noreply.github.com>

---
+++ setup-dnsmasq.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+brew install dnsmasq
+echo 'address=/.localhost/127.0.0.1' >> $(brew --prefix)/etc/dnsmasq.conf
+echo 'port=53' >> $(brew --prefix)/etc/dnsmasq.conf
+sudo brew services start dnsmasq
+sudo mkdir -v /etc/resolver
+sudo echo 'nameserver 127.0.0.1' >> /etc/resolver/localhost
